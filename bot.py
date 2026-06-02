615648dd0e29542ca51c18929cd329736c3e82a8
Vsevolod Shimko <vsevolodshimko@MacBook-Pro-Vsevolod.local>
2026-06-02 15:48:29 +0300

    rating commands

--- bot.py
+++ bot.py
@@ -154,4 +154,95 @@ app.add_handler(
 
 print("Bot started")
 
+app.add_handler(
+    CommandHandler("gold1v1", gold1v1)
+)
+
+app.add_handler(
+    CommandHandler("silver1v1", silver1v1)
+)
+
+app.add_handler(
+    CommandHandler("bronze1v1", bronze1v1)
+)
+
+app.add_handler(
+    CommandHandler("gold2v2", gold2v2)
+)
+
+app.add_handler(
+    CommandHandler("silver2v2", silver2v2)
+)
+
+app.add_handler(
+    CommandHandler("bronze2v2", bronze2v2)
+)
 app.run_polling()
+def save_ratings(data):
+    with open("ratings.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+async def add_medal(update: Update,
+                    context: ContextTypes.DEFAULT_TYPE,
+                    mode,
+                    medal):
+
+    ADMIN_ID = 509816654
+
+    if update.effective_user.id != ADMIN_ID:
+        return
+
+    if not context.args:
+        await update.message.reply_text(
+            "Использование:\n/gold1v1 @username"
+        )
+        return
+
+    username = context.args[0]
+
+    if not username.startswith("@"):
+        username = "@" + username
+
+    ratings = load_ratings()
+
+    season = ratings["25/26"]
+
+    if username not in season[mode]:
+        season[mode][username] = {
+            "gold": 0,
+            "silver": 0,
+            "bronze": 0
+        }
+
+    season[mode][username][medal] += 1
+
+    save_ratings(ratings)
+
+    await update.message.reply_text(
+        f"✅ {username} получил {medal}"
+    )
+
+
+async def gold1v1(update, context):
+    await add_medal(update, context, "solo", "gold")
+
+
+async def silver1v1(update, context):
+    await add_medal(update, context, "solo", "silver")
+
+
+async def bronze1v1(update, context):
+    await add_medal(update, context, "solo", "bronze")
+
+
+async def gold2v2(update, context):
+    await add_medal(update, context, "duo", "gold")
+
+
+async def silver2v2(update, context):
+    await add_medal(update, context, "duo", "silver")
+
+
+async def bronze2v2(update, context):
+    await add_medal(update, context, "duo", "bronze")
